04af6e347397628f3c6c0357202add789755054b
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-08-20 11:33:41 +0900

    Refactor: 변경 findBook
    페치조인 사용하도록 오버라이딩

--- src/main/java/mangpo/server/repository/BookRepository.java
+++ src/main/java/mangpo/server/repository/BookRepository.java
@@ -2,13 +2,20 @@ package mangpo.server.repository;
 
 import mangpo.server.entity.Book;
 import mangpo.server.entity.BookCategory;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByCategory(BookCategory category);
+
     Book findByIsbn(String isbn);
+
+    @Override
+    @EntityGraph(attributePaths = ("posts"))
+    Optional<Book> findById(Long id);
 
 }
