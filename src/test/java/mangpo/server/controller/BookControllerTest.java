22edd051346c4c3cc611659eb3f36d03008dabbc
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-02-21 19:31:10 +0900

    Feat: 추가 getTotalBook
    유저의 총 책 개수 반환

--- src/test/java/mangpo/server/controller/BookControllerTest.java
+++ src/test/java/mangpo/server/controller/BookControllerTest.java
@@ -1,10 +1,10 @@
-package mangpo.server.controller;
-
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebMvcTest(BookController.class)
-class BookControllerTest {
-
-}
+//package mangpo.server.controller;
+//
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@WebMvcTest(BookController.class)
+//class BookControllerTest {
+//
+//}
