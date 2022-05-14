b905a71998f31b74bd20641782d6d4ca58cc0958
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-05-14 15:59:59 +0900

    Feat: 추가 InviteResponse.clubName

--- src/main/java/mangpo/server/dto/club/InviteResponseDto.java
+++ src/main/java/mangpo/server/dto/club/InviteResponseDto.java
@@ -8,12 +8,14 @@ public class InviteResponseDto {
     private Long inviteId;
     private String userEmailToInvite;
     private Long clubId;
+    private String clubName;
     private String message;
 
     public InviteResponseDto(Invite invite){
         this.inviteId = invite.getId();
         this.userEmailToInvite = invite.getUser().getEmail();
         this.clubId = invite.getClub().getId();
+        this.clubName = invite.getClub().getName();
         this.message = invite.getMessage();
     }
 }
