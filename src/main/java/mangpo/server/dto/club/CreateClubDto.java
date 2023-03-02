c37fb5e3dedc786cfbf159b35e9f15f4eadbc069
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2023-03-02 14:16:13 +0900

    Test: 변경 테스트코드
    불필요한 코드 제거

--- src/main/java/mangpo/server/dto/club/CreateClubDto.java
+++ src/main/java/mangpo/server/dto/club/CreateClubDto.java
@@ -1,8 +1,12 @@
 package mangpo.server.dto.club;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateClubDto {
     private String name;
     private String description;
